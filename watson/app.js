40c920e408a2a6060059d636f807839daa0b2859
Maria <mariaanguevara@gmail.com>
2018-10-20 15:06:31 -0500

    camara funcional

---
+++ watson/app.js
@@ -0,0 +1,45 @@
+
+window.addEventListener("DOMContentLoaded",  ()=> {
+    const video = document.getElementById('video');
+    const localStream = null;
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const errBack = (e)=> {
+        console.log('Opps.. no se puede utilizar la cámara', e);
+    };
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then( (stream)=> {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        });
+    } else if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia({
+            video: true
+        },  (stream)=> {
+            video.src = stream;
+            video.play();
+            localStream = stream;
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia({
+            video: true
+        },  (stream) =>{
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            localStream = stream;
+        }, errBack);
+    } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
+        navigator.mozGetUserMedia({
+            video: true
+        },  (stream)=> {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            localStream = stream;
+        }, errBack);
+    }
+    document.getElementById('tomar').addEventListener('click', ()=> {
+        context.drawImage(video, 0, 0, 250, 300);
+    });
+}, false);
