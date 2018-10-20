770b5d501b0be79c5befa1c524d4e73891cf370e
Maria <mariaanguevara@gmail.com>
2018-10-20 16:00:48 -0500

    division primera y segunda parte

--- watson/app.js
+++ watson/app.js
@@ -1,23 +1,26 @@
-
-window.addEventListener("DOMContentLoaded",  ()=> {
+const seeFirst = document.getElementById('fisrt');
+const seeSecond = document.getElementById('second');
+const btnReturn = document.getElementById('return');
+const btnCamera = document.getElementById('btn-camera');
+window.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById('video');
     const localStream = null;
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
-    const errBack = (e)=> {
+    const errBack = (e) => {
         console.log('Opps.. no se puede utilizar la cámara', e);
     };
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({
             video: true
-        }).then( (stream)=> {
+        }).then((stream) => {
             video.src = window.URL.createObjectURL(stream);
             video.play();
         });
     } else if (navigator.getUserMedia) { // Standard
         navigator.getUserMedia({
             video: true
-        },  (stream)=> {
+        }, (stream) => {
             video.src = stream;
             video.play();
             localStream = stream;
@@ -25,7 +28,7 @@ window.addEventListener("DOMContentLoaded",  ()=> {
     } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
         navigator.webkitGetUserMedia({
             video: true
-        },  (stream) =>{
+        }, (stream) => {
             video.src = window.URL.createObjectURL(stream);
             video.play();
             localStream = stream;
@@ -33,13 +36,22 @@ window.addEventListener("DOMContentLoaded",  ()=> {
     } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
         navigator.mozGetUserMedia({
             video: true
-        },  (stream)=> {
+        }, (stream) => {
             video.src = window.URL.createObjectURL(stream);
             video.play();
             localStream = stream;
         }, errBack);
     }
-    document.getElementById('tomar').addEventListener('click', ()=> {
+    document.getElementById('tomar').addEventListener('click', () => {
         context.drawImage(video, 0, 0, 250, 300);
     });
 }, false);
+// visualizacion de secciones
+btnCamera.addEventListener('click', () => {
+    seeFirst.setAttribute('class', 'hidden');
+    seeSecond.removeAttribute('class');
+})
+btnReturn.addEventListener('click', () => {
+    seeSecond.setAttribute('class', 'hidden');
+    seeFirst.removeAttribute('class');
+})
