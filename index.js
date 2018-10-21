b033d7d679189005e0f97725403bdff862251ee3
Maria <mariaanguevara@gmail.com>
2018-10-20 23:40:59 -0500

    alternativas de formulario

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const seeFirst = document.getElementById('fisrt');
 const seeSecond = document.getElementById('second');
 const seeThird = document.getElementById('third');
+const seeEyes = document.getElementById('eyes');
+const seeNotEyes = document.getElementById('not-eyes');
 const btnReturn = document.getElementById('return-img');
 const btnReturnFrom = document.getElementById('return-from');
 const btnCamera = document.getElementById('btn-camera');
 const btnForm = document.getElementById('btn-form');
+const btnYes = document.getElementById('yes');
+const btnNot = document.getElementById('not');
+
+// variables de preguntas
+const questionOne=document.getElementById('Q1');
+const questiontwo=document.getElementById('Q2');
+const questionthree=document.getElementById('Q3');
+const questionfour=document.getElementById('Q4');
+const questionfive=document.getElementById('Q5');
+const questionsix=document.getElementById('Q6');
+const questionseven=document.getElementById('Q7');
+
 
 window.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById('video');
@@ -67,3 +81,22 @@ btnReturnFrom.addEventListener('click', () => {
     seeThird.setAttribute('class', 'hidden');
     seeFirst.removeAttribute('class');
 })
+btnYes.addEventListener('click', () => {
+    if(seeEyes.hasAttribute('class')){
+        seeEyes.removeAttribute('class');
+        seeNotEyes.setAttribute('class', 'hidden');
+    }
+    else{
+        seeEyes.setAttribute('class', 'hidden');
+    }
+})
+btnNot.addEventListener('click', () => {
+    if(seeNotEyes.hasAttribute('class')){
+        seeNotEyes.removeAttribute('class');
+        seeEyes.setAttribute('class', 'hidden');
+    }
+    else{
+        seeNotEyes.setAttribute('class', 'hidden');
+    }
+})
+// 
