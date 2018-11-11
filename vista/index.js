fa49d9a8a953dd49049c91322c47c34777604876
Maria Noriega <mariaanguevara@gmail.com>
2018-11-11 18:16:23 -0500

    subir

--- vista/index.js
+++ vista/index.js
@@ -4,6 +4,7 @@ const seeThird = document.getElementById('third');
 const seeEyes = document.getElementById('eyes');
 const seeNotEyes = document.getElementById('not-eyes');
 const seeEvaluate = document.getElementById('evaluate');
+const seeEvaluateImg = document.getElementById('evaluate-img');
 const btnReturn = document.getElementById('return-img');
 const btnReturnFrom = document.getElementById('return-from');
 const btnCamera = document.getElementById('btn-camera');
@@ -12,6 +13,7 @@ const btnYes = document.getElementById('yes');
 const btnNot = document.getElementById('not');
 const modal = document.getElementById('myModal');
 const span = document.getElementsByClassName("off")[0];
+const canvas = document.getElementById('canvas');
 
 
 // variables de preguntas
@@ -27,7 +29,6 @@ const eyeNumber = document.getElementById('eye-number');
 window.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById('video');
     const localStream = null;
-    const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
     const errBack = (e) => {
         console.log('Opps.. no se puede utilizar la cámara', e);
@@ -66,6 +67,8 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     document.getElementById('tomar').addEventListener('click', () => {
         context.drawImage(video, 0, 0, 250, 300);
+
+
     });
 }, false);
 // visualizacion de secciones
@@ -103,6 +106,8 @@ btnNot.addEventListener('click', () => {
 })
 // Funcion de calculo
 const data = (Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8) => {
+    console.log(Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8);
+
     const obj = {
         p1: '',
         p2: '',
@@ -114,6 +119,7 @@ const data = (Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8) => {
         p8: ''
     }
     if (Q8 === '') {
+
         obj.p1 = (15.71) * (Q1);
         obj.p2 = (10.71) * (Q2);
         obj.p3 = (10.71) * (Q3);
@@ -123,7 +129,8 @@ const data = (Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8) => {
         obj.p7 = (15.71) * (Q7);
         obj.p8 = 0;
         console.log(obj);
-    } if(Q8 !== '') {
+    }
+    if (Q8 !== '') {
         obj.p1 = (10) * (Q1);
         obj.p2 = (5) * (Q2);
         obj.p3 = (5) * (Q3);
@@ -141,21 +148,74 @@ seeEvaluate.addEventListener('click', () => {
     const good = document.getElementById('cool');
     const soSo = document.getElementById('so-so');
     const bad = document.getElementById('bad');
-    const suma = data(parseInt(questionOne.value), parseInt(questiontwo.value), parseInt(questionthree.value), parseInt(questionfour.value), parseInt(questionfive.value), parseInt(questionsix.value), parseInt(questionseven.value),eyeNumber.value);
+    const suma = data(parseInt(questionOne.value), parseInt(questiontwo.value), parseInt(questionthree.value), parseInt(questionfour.value), parseInt(questionfive.value), parseInt(questionsix.value), parseInt(questionseven.value), eyeNumber.value) / 5;
     console.log(suma);
-    
-    if (suma<33) {
+
+    if (suma < 33) {
         good.removeAttribute('class');
+        soSo.setAttribute('class', 'hidden');
+        bad.setAttribute('class', 'hidden');
         console.log('1');
     }
-    if (33<suma<66) {
+    if (33 < suma && suma < 66) {
         console.log('2');
-        
         soSo.removeAttribute('class');
+        good.setAttribute('class', 'hidden');
+        bad.setAttribute('class', 'hidden');
     }
-    if (66<suma) {
+    if (66 < suma) {
         console.log('3');
-        
         bad.removeAttribute('class');
+        good.setAttribute('class', 'hidden');
+        soSo.setAttribute('class', 'hidden');
     }
+})
+// fujo de funcionamiento
+
+
+seeEvaluateImg.addEventListener('click', () => {
+    const canvas = document.getElementById('canvas');
+    const good = document.getElementById('cool-img');
+    const soSo = document.getElementById('so-so-img');
+    const bad = document.getElementById('bad-img');
+
+    const img = canvas.toDataURL();
+    var data = {
+        imagenB64: img
+    }
+
+    $.ajax({
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        url: '/obtenerResultVR',
+        type: 'POST',
+        beforeSend: function () {
+            $("#score").val("Procesando, espere por favor...");
+        },
+        success: function (response) {
+            const data = response.images[0].classifiers[0].classes[0].score;
+            if(data<=0.3){
+                good.removeAttribute('class');
+                soSo.setAttribute('class', 'hidden');
+                bad.setAttribute('class', 'hidden');
+
+            }else if(data>=0.4 &&data<=0.7){
+                soSo.removeAttribute('class');
+                good.setAttribute('class', 'hidden');
+                bad.setAttribute('class', 'hidden');
+
+            }else if(data>=0.7){
+                bad.removeAttribute('class');
+                soSo.setAttribute('class', 'hidden');
+                good.setAttribute('class', 'hidden');
+
+            }else{
+                alert('toma la foto de nuevo')
+
+            }
+            console.log(response)
+            // $("#score").val(response);
+        }
+    });
 })
