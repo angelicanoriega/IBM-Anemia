96c62b16fbdb4a6c9270d0b8f31d6a8d3147d985
Maria Noriega <mariaanguevara@gmail.com>
2018-11-11 12:55:40 -0500

    arreglo

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
-<<<<<<< HEAD
-const express = require('express') ,
- bodyParser = require('body-parser');
-=======
 const express = require('express'),
     bodyParser = require('body-parser');
->>>>>>> 966b97c9aca836867567e8629cd88b96f0130616
 const cfenv = require('cfenv');
 var base64Img = require('base64-img');
 var port = process.env.PORT || 8081;
@@ -22,27 +17,6 @@ app.use(bodyParser.json());
 
 app.post("/obtenerResultVR", function (req, response) {
 
-<<<<<<< HEAD
-    var imagenB64  = req.body.imagenB64;
-    base64Img.img(imagenB64, './public/imgs/', 'imagen', function(err, filepath) {
-        console.log(filepath)
-    })
-    const images_file = fs.createReadStream("./public/imgs/imagen.png");
-
-    const params = {
-        images_file: images_file,
-        classifier_ids: ["descarte_1717387835"]
-    };
-    visualRecognition.classify(params, function (err, res) {
-        if (err) {
-            console.log(err);
-        } else {
-
-            // const data = JSON.stringify(res.images[0].classifiers[0].classes[0].score);
-            response.json(res);
-        }
-    });
-=======
     var imagenB64 = req.body.imagenB64;
     base64Img.img(imagenB64, './public/imgs/', 'imagen', function (err, filepath) {
         console.log("b64 to img file")
@@ -66,7 +40,6 @@ console.log("enviando img")
     })
 
 
->>>>>>> 966b97c9aca836867567e8629cd88b96f0130616
 
 
 });
